392b1f05d00d6508195d8d58f19995988ce0b4f2
grisha <codpure@gmail.com>
2021-05-27 14:21:00 +0300

    put back to running shoes term

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     crawlUrl: 'https://www.ebay.com/sch/i.html?_nkw=',
-    searchTerm: 'smart phones',
+    searchTerm: 'running shoes',
     elements: {
         relatedSearches: '.srp-related-searches',
         productResults: '.srp-results.clearfix',
